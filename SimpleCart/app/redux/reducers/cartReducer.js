ee6be400cd05cfb7bc3d1758d8839ae096ce0c38
Tanmoy <tanmoy@quanticdynamics.com>
2022-06-15 00:41:54 +0600

    added images

--- SimpleCart/app/redux/reducers/cartReducer.js
+++ SimpleCart/app/redux/reducers/cartReducer.js
@@ -1,9 +1,12 @@
-import Item1 from './../../assets/images/item1.jpg'
-import Item2 from './../../assets/images/item2.jpg'
-import Item3 from './../../assets/images/item3.jpg'
-import Item4 from './../../assets/images/item4.jpg'
-import Item5 from './../../assets/images/item5.jpg'
-import Item6 from './../../assets/images/item6.jpg'
+import Item1 from './../../assets/images/item1.jpeg'
+import Item2 from './../../assets/images/item2.jpeg'
+import Item3 from './../../assets/images/item3.jpeg'
+import Item4 from './../../assets/images/item4.jpeg'
+import Item5 from './../../assets/images/item5.jpeg'
+import Item6 from './../../assets/images/item6.jpeg'
+import Item7 from './../../assets/images/item7.jpeg'
+import Item8 from './../../assets/images/item8.jpeg'
+import Item9 from './../../assets/images/item9.jpeg'
 import { ADD_TO_CART, REMOVE_ITEM, SUB_QUANTITY, ADD_QUANTITY, ADD_SHIPPING } from './../actions/action-types/cart-actions'
 
 
@@ -14,7 +17,10 @@ const initState = {
         { id: 3, title: 'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 120, img: Item3 },
         { id: 4, title: 'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 260, img: Item4 },
         { id: 5, title: 'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 160, img: Item5 },
-        { id: 6, title: 'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 90, img: Item6 }
+        { id: 6, title: 'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 90, img: Item6 },
+        { id: 7, title: 'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 260, img: Item7 },
+        { id: 8, title: 'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 160, img: Item8 },
+        { id: 9, title: 'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 90, img: Item9 }
     ],
     addedItems: [],
     total: 0
